f89555db7e35ea9c2136a4038a61ea92cbe4bf9f
Owoeye Samuel <owosamuel8@gmail.com>
2024-08-25 01:26:49 +0100

    Update of Interactive quiz and name changed to Meta quiz

--- index.js
+++ index.js
@@ -7,11 +7,39 @@ const _dirname = dirname(fileURLToPath(import.meta.url));
 const port = 3000;
 const app = express();
 
-app.use(express.static('docs'));  
+let firstName = '';
+let lastName = '';
+let email = '';
+let password = '';
+let loginEmail = '';
+let loginPassword = '';
+
+app.set('view engine', 'ejs');
+app.use(express.static('public')); 
+app.use(bodyParser.urlencoded({ extended: true }));  
 
 app.get('/', (req, res) => {
-    res.sendFile(_dirname + '/docs/index.html');
-});
+    res.render('signUp.ejs',)
+  });
+
+  app.post('/sign-up', (req, res) => {
+    firstName = req.body['first-name'];
+    lastName = req.body['last-name'];
+    email = req.body['email'];
+    password = req.body['password'];
+    res.render('logIn.ejs', { errorMessage: '' });
+  })
+
+  app.post('/log-in', (req, res) => {
+    loginEmail = req.body['login-email'];
+    loginPassword = req.body['login-password'];
+    if (email === loginEmail && password === loginPassword) {
+        res.render('question.ejs');
+    } else {
+        res.render('logIn.ejs', { errorMessage: '<p>Wrong email or password!</p>'});
+
+    }
+  })
 
 app.listen(port, () => { 
     console.log(`Server running on port ${port}.`)
